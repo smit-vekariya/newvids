590f4e471d4cf7ec4410096b1671cd30fe64392f
smit <smitvekariya164@gmail.com>
2024-04-29 23:26:24 +0530

    add new data

--- mysite/apis/views.py
+++ mysite/apis/views.py
@@ -10,15 +10,18 @@ import urllib.request
 class AddData(APIView):
     authentication_classes = []
     permission_classes = []
-  
+
     def get(self, request):
-        data = {"userId": "Y1eID7Cv00d4amwRQWrMAQ==", "app_type": "ios", "pagecode": 0, "ver_code": 1.0, "accessToken": "910dfdfda1a3c2153008551a55327cd0", "api_unix": "1713807882"} 
-        response = requests.post("https://newvids.xyz/wowstory/ptmkr/posterdata", data, timeout=10)
-        with transaction.atomic():
+        for i in range(49,50+1):
+            print(f"======================= >start page number ({i}) <===========================")
+            data = {"userId": "Y1eID7Cv00d4amwRQWrMAQ==", "app_type": "ios", "pagecode": i, "ver_code": 1.0, "accessToken": "910dfdfda1a3c2153008551a55327cd0", "api_unix": "1713807882"}
+            response = requests.post("https://newvids.xyz/wowstory/ptmkr/posterdata", data, timeout=10)
             if response.status_code == 200:
                 response = response.json()
+                print("response", response)
                 if response["msgcode"] == 1:
                     for data in response["poster_data"]:
+                        print(f">>>> start cat_id ({data['cat_id']}) on page ({i})")
                         poster_data ={}
                         poster_data["cat_id"] = data["cat_id"]
                         poster_data["cat_name"] = data["cat_name"]
@@ -27,7 +30,6 @@ class AddData(APIView):
                             poster_instance = poster_serializer.save()
                         else:
                             raise Exception(f"{poster_serializer.errors}")
-                            return Response(f"Error while valid data for poster model!")
                         for temp_data  in data["template_data"]:
                             templete_data = temp_data
                             templete_data["poster_id"] = poster_instance.id
@@ -36,11 +38,12 @@ class AddData(APIView):
                                 templete_serializers.save()
                             else:
                                 raise Exception(f"{templete_serializers.errors}")
-                                return Response(f"Error while valid data for template model!")
                 else:
                     return Response(f"Something went wrong. msgcode getting {response['msgcode']}.")
             else:
                 return Response("Something went wrong while getting data from newvids!")
+            print(f"======================= >finish page number ({i}) <===========================")
         return Response("Data enter successfully")
 
 # https://8000-monospace-newvids-1713848878294.cluster-7ubberrabzh4qqy2g4z7wgxuw2.cloudworkstations.dev/apis/add_data
+# http://192.168.29.175:8000/apis/add_data
